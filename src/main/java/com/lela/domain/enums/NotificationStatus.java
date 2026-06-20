6edebe314438b07d7a713dad57925f16911e492c
vanbaokhang224 <vanbaokhang224@gmail.com>
2026-06-20 15:43:06 +0700

    delete

--- src/main/java/com/lela/domain/enums/NotificationStatus.java
+++
@@ -1,9 +0,0 @@
-package com.lela.domain.enums;
-
-// Enum trạng thái gửi thông báo.
-public enum NotificationStatus {
-    PENDING, // Chờ gửi.
-    SENT, // Đã gửi thành công.
-    FAILED, // Gửi thất bại.
-    CANCELLED // Đã hủy gửi.
-}
--- src/main/java/com/lela/domain/enums/ReviewSessionType.java
+++
@@ -1,8 +0,0 @@
-package com.lela.domain.enums;
-
-// Enum loại phiên review.
-public enum ReviewSessionType {
-    REGULAR, // Phiên review thông thường.
-    CRAM, // Phiên ôn cấp tốc.
-    LEARN_NEW // Phiên học thẻ mới.
-}
