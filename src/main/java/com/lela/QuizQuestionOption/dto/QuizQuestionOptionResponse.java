164c0314a30b076ca2dd8033848ae563cd535b5e
truong duy <hieunguyen369248pro@gmail.com>
2026-06-21 11:31:51 +0700

    commit

--- src/main/java/com/lela/QuizQuestionOption/dto/QuizQuestionOptionResponse.java
+++ src/main/java/com/lela/QuizQuestionOption/dto/QuizQuestionOptionResponse.java
@@ -3,7 +3,7 @@ package com.lela.QuizQuestionOption.dto;
 import lombok.Data;
 import java.time.LocalDateTime;
 import java.math.BigDecimal;
-import com.lela.QuestionType.domain.QuestionType;
+
 import com.lela.QuizAttemptStatus.domain.QuizAttemptStatus;
 import com.lela.QuizQuestionOption.domain.QuizQuestionOption;
 
